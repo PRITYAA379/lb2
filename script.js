6fb2626875ec7a748f8f0e613b1384225fe15bb8
Preet <JAGTAPPRITAM73@GMAIL.COM>
2025-11-24 21:18:42 +0530

    Add function to open WhatsApp with predefined message

---
+++ script.js
@@ -0,0 +1,13 @@
+function openWhatsApp() {
+    const msg = `Hi, I want intraday clarity.
+I'm looking for clean, noise-free guidance.
+I need levels that are clear and simple to follow.
+I want someone experienced I can rely on.
+I prefer discipline over risky calls.
+Connect with me and guide me properly.`;
+
+    window.open(
+        "https://wa.me/918828657392?text=" + encodeURIComponent(msg),
+        "_blank"
+    );
+}
